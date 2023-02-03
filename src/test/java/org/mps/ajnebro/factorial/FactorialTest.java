7218e1ccd71692335b5347ef0fa49788d7779a82
ajnebro <ajnebro@users.noreply.github.com>
2023-02-03 12:28:02 +0100

    Refactor test cases

--- src/test/java/org/mps/ajnebro/factorial/FactorialTest.java
+++ src/test/java/org/mps/ajnebro/factorial/FactorialTest.java
@@ -1,5 +1,8 @@
 package org.mps.ajnebro.factorial;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,9 +17,20 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 
 class FactorialTest {
+  Factorial factorial  ;
+
+  @BeforeEach
+  void setup() {
+    factorial = new Factorial() ;
+  }
+
+  @AfterEach
+  void shutdown() {
+    factorial = null ;
+  }
+
   @Test
   void factorialOfZeroIsOne() {
-    var factorial = new Factorial() ;
     int obtainedValue = factorial.compute(0) ;
     int expectedValue = 1 ;
 
@@ -25,7 +39,6 @@ class FactorialTest {
 
   @Test
   void factorialOfOneIsOne() {
-    var factorial = new Factorial() ;
     int obtainedValue = factorial.compute(1) ;
     int expectedValue = 1 ;
 
@@ -34,7 +47,6 @@ class FactorialTest {
 
   @Test
   void factorialOfTwoIsTwo() {
-    var factorial = new Factorial() ;
     int obtainedValue = factorial.compute(2) ;
     int expectedValue = 2 ;
 
@@ -43,7 +55,6 @@ class FactorialTest {
 
   @Test
   void factorialOf3Is6() {
-    var factorial = new Factorial() ;
     int obtainedValue = factorial.compute(3) ;
     int expectedValue = 6 ;
 
@@ -52,7 +63,6 @@ class FactorialTest {
 
   @Test
   void factorialOf5Is120() {
-    var factorial = new Factorial() ;
     int obtainedValue = factorial.compute(5) ;
     int expectedValue = 120 ;
 
