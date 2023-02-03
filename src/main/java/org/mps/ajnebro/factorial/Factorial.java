062cd698c18d959e0dfa21b3de7e7736c1f51967
ajnebro <ajnebro@users.noreply.github.com>
2023-02-03 12:35:26 +0100

    Refactor class Factorial

--- src/main/java/org/mps/ajnebro/factorial/Factorial.java
+++ src/main/java/org/mps/ajnebro/factorial/Factorial.java
@@ -3,10 +3,8 @@ package org.mps.ajnebro.factorial ;
 public class Factorial {
   public int compute(int value) {
     int result ;
-    if ((value == 0) || (value == 1)) {
+    if (value == 0) {
       result = 1 ;
-    } else if (value == 2){
-      result = 2 ;
     } else {
       return value * compute(value-1) ;
     }
